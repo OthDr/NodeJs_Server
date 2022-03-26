9c7443dfd2092e791c3189c0027d9a1e89e570ba
OthDr <otmax.vft@gmail.com>
2022-03-26 22:31:01 +0100

    Project added

---
+++ Server.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const fs = require('fs');
+const { log } = require('console');
+
+const server = http.createServer((req,res)=>
+    {    
+        console.log('request made\n',req.url , req.method);
+        res.setHeader('Content-Type','text/html');
+        let path = './Views/';
+
+        // ------- simple routing ----------
+        switch(req.url){
+            case '/':
+                path += 'index.html';
+                res.statusCode = 200;
+                break;
+            case '/login':
+                path += 'Login.html';
+                res.statusCode = 200;
+                break;
+            case '/about':
+                path += 'About.html';
+                res.statusCode = 200;
+                break;   
+            default:
+                path += '404.html';
+                res.statusCode = 404;
+                break;             
+        }
+        // ----------  Reading & Sending : index.html ------------
+
+        fs.readFile(path,(err,data)=>{
+            if(err){
+                console.log(err);
+                res.end();
+            }else{
+                console.log('html successfully sent');
+                //res.write(data);
+                res.end(data); // since it's only a one line then this is similar as above
+            }
+        });
+
+        /*res.setHeader('content-type','text/html');
+        res.write('<h3 style="color: blueviolet;margin-left: 43%;">This is a response</h3>');
+        res.end();*/
+
+    }).listen(3000,'localhost' , ()=>
+    {
+        console.log('Server is listening on port 3000')
+    });
+
